20adfaf5198bda3ad76cb5134f869998d546eaff
amirjcr <Amir_jcr2@yahoo.com>
2022-02-13 10:11:36 +0330

    Initial Project

---
+++ CRMNotification/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CRMNotification
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private static IServiceProvider _serviceProvider;
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                _serviceProvider = new ServiceCollection()
+                                      .BuildServiceProvider();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ InfrastrucureLayer/SqlConnectionHandler/SqlConnectionHelper.cs
@@ -0,0 +1,56 @@
+﻿using ApplicationLayer.SqlConnectionHandler;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastrucureLayer.SqlConnectionHandler
+{
+    public class SqlConnectionHelper: ISqlConnectionHandler
+    {
+        private static object _lock = new object();
+        private SqlConnection _connection;
+        private static string _connectionString;
+        private bool _disposed = false;
+
+        public SqlConnectionHelper(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+                throw new ArgumentNullException($"{typeof(string) + " " + nameof(connectionString)} can not be null while passing as the paramter in {typeof(SqlConnection)}");
+
+            _connectionString = connectionString;
+        }
+
+
+        public SqlConnection Create()
+        {
+            lock (_lock)
+            {
+                if (_connection == null)
+                    _connection = new SqlConnection(_connectionString);
+
+                return _connection;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool dispose)
+        {
+            if (_disposed) return;
+            if (dispose)
+            {
+                _connection?.Dispose();
+                _connectionString = null;
+            }
+
+            _disposed = true;
+        }
+    }
+}
