f95e8ed51f8ee23cf08584abe525f71c27f41040
amirjcr <51033271+amirjcr@users.noreply.github.com>
2022-02-15 21:34:14 +0330

    Added WPF UI to the Application And Create ViewModel Folder With BaseModel and Some Wpf Window and Add Some Fonts the Application

---
+++ MahdeFooladWPF/ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MahdeFooladWPF.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        // Fire the Property Change Event when the data has been changed.
+        protected virtual void OnPropertyChaned(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ MahdeFooladWPF/ViewModels/TaskViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MahdeFooladWPF.ViewModels
+{
+    internal class TaskViewModel : BaseViewModel
+    {
+
+        private string _activityId;
+        public string ActivityId
+        {
+            get => _activityId;
+            set { _activityId = value; OnPropertyChaned(nameof(ActivityId)); }
+        }
+    }
+}
