a5a6dc65d31987e010f740def25428e42edce883
amirjcr <51033271+amirjcr@users.noreply.github.com>
2022-02-16 21:51:17 +0330

    Added ViewModel Folder to the project

---
+++ NSMangament.Application/ViewModels/testclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSMangament.Application.ViewModels
+{
+
+    // created for the reminder perpose and should be impelemnted with the corroct 
+    internal class testclass
+    {
+    }
+}
