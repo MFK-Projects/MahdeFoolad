b3fa018ee1e6d42e1c3d9a587901bd14904de0a5
amirjcr <51033271+amirjcr@users.noreply.github.com>
2022-02-17 22:12:19 +0330

    static url builder classs are defined

--- MahdeFooald.Common/RequestUrl.cs
+++ MahdeFooald.Common/RequestUrl.cs
@@ -6,24 +6,50 @@ using System.Threading.Tasks;
 
 namespace MahdeFooald.Common
 {
-   public static class RequestUrl
+    public static class RequestUrl
     {
         private const string baseurl = "http://crm-srv:8585/MFKian/api/data/v9.0/";
         private const string taskurl = "http://crm-srv:8585/MFkian/main.aspx?etn=";
 
-        public static string CrmSettingUrl()
-        {
-            return "";
-        }
 
-        public static string CrmTasksUrl(string userId  )
-        {
-            return string.Empty;
-        }
+        private const string _taskSelectedItem = @"activityid,new_remained_time_hour,
+                                        new_remaining_days,new_task_status,new_task_type,subject,prioritycode,
+                                        description,new_telephone,_new_date_deadline_value,_ownerid_value";
 
-        public static string CrmTaskUrl(string taskId)
+
+        private const string _settingCrmSelectedItem = @"new_api_url,new_baseurl,new_day_filter,new_domain_name,new_entity_name,
+                                                         new_expired_task_code,new_log_path,new_name,new_notification_message,
+                                                         new_notification_systemid,new_setting_timer,new_task_api,new_task_status,
+                                                         new_task_types,new_time_awaited,new_time_check";
+
+        private const string _userSelectedItem = "fullname,domainname,identityid";
+
+
+        public static string BuildUrl(UrlBuilderMode mode, string userId, string tasksId)
         {
-            return string.Empty;
+            if (mode == UrlBuilderMode.SingleTask && string.IsNullOrEmpty(tasksId))
+                throw new ArgumentNullException("userid or taskid sent null to the method ");
+            else if (mode == UrlBuilderMode.MultipuleTasks && string.IsNullOrEmpty(userId))
+                throw new ArgumentNullException("userid or taskid sent null to the method ");
+
+            return mode switch
+            {
+                UrlBuilderMode.Setting => $"{baseurl}new_notification_systems?$select={_settingCrmSelectedItem}",
+                UrlBuilderMode.SingleTask => $"{taskurl}task&id={tasksId}&pagetype=entityrecord#",
+                UrlBuilderMode.MultipuleTasks => $"{baseurl}tasks?$select={_taskSelectedItem}&$filter=_ownerid_value eq {userId}",
+                UrlBuilderMode.SingleUser => $"{baseurl}systemusers?$select={_userSelectedItem}&$filter=domainname eq '{userId}'",
+                _ => throw new ArgumentOutOfRangeException()
+            };
         }
     }
+
+    public enum UrlBuilderMode
+    {
+        MultipuleTasks,
+        SingleUser,
+        Setting,
+        SingleTask
+    }
 }
+
+
